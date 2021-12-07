7520fe12bfcb9338cb97924c56397ecec837f045
Loic Huder <loic.huder@esrf.fr>
2021-12-07 09:23:05 +0100

    Test CI

--- tests/EDD/test_graphs.py
+++ tests/EDD/test_graphs.py
@@ -24,6 +24,7 @@ def edd_graph(config):
 def test_edd_graph(tmp_path: Path):
     test_data_path, config = calib_edd_init(tmp_path)
     graph = edd_graph(config)
+    config = config_7
     results = execute_graph(graph)
     assert len(results) == 1
     for node_id, task in results.items():
