88400bd55f833451ba9911f8957bda31036ea454
Loic Huder <loic.huder@esrf.fr>
2022-02-11 15:26:52 +0100

    Use same method to compute scattering directions

--- easistrain/EDD/strainStressd0cstEDD.py
+++ easistrain/EDD/strainStressd0cstEDD.py
@@ -2,44 +2,8 @@ from typing import Sequence, Tuple
 import numpy as np
 import h5py
 import scipy.optimize
+from easistrain.EDD.math import compute_qs
 from easistrain.EDD.utils import run_from_cli
-
-
-def angles_to_rad(angles: np.ndarray):
-    rad_angles = np.radians(angles)
-    # Convert 2*theta in theta
-    rad_angles[:, 4] = 0.5 * rad_angles[:, 4]
-
-    # phi, chi, omega, delta, 2*theta as columns
-    return np.transpose(rad_angles)
-
-
-def compute_qs(angles: np.ndarray):
-    rad_angles = angles_to_rad(angles)
-    cos_phi, cos_chi, cos_omega, cos_delta, cos_theta = np.cos(rad_angles)
-    sin_phi, sin_chi, sin_omega, sin_delta, sin_theta = np.sin(rad_angles)
-    q1 = (
-        (cos_theta * cos_chi * sin_delta * sin_phi)
-        + (
-            cos_delta
-            * cos_theta
-            * ((cos_phi * sin_omega) - (cos_omega * sin_phi * sin_chi))
-        )
-        - sin_theta * ((cos_phi * cos_omega) + (sin_phi * sin_chi * sin_omega))
-    )
-    q2 = (
-        cos_delta
-        * cos_theta
-        * ((cos_phi * cos_omega * sin_chi) + (sin_phi * sin_omega))
-        - (cos_theta * cos_phi * cos_chi * sin_delta)
-        - (sin_theta * ((cos_omega * sin_phi) - (cos_phi * sin_chi * sin_omega)))
-    )
-    q3 = (
-        (cos_delta * cos_theta * cos_chi * cos_omega)
-        + (cos_theta * sin_delta * sin_chi)
-        + (cos_chi * sin_theta * sin_omega)
-    )
-    return q1, q2, q3
 
 
 def guess_strain(
